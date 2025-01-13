293cb48fd553cee70615449b2a9c4776db8340a7
jezeus17 <jesuscastellanosreynaldo@gmail.com>
2025-01-13 18:55:35 -0500

    test

--- src/modules/management/test/views/components/AssignTest.vue
+++ src/modules/management/test/views/components/AssignTest.vue
@@ -1,14 +1,6 @@
 <template>
   <div flex flex-col gap-2>
-    <span><span text-primary>Nombres: </span>{{ test.name }}</span>
-    <span><span text-primary>Descripción: </span>{{ test.description }}</span>
-    <span><span text-primary>Tiempo de Duración (min): </span>{{ test.time_duration }}</span>
-    <span><span text-primary>Tiempo para poder ser ejecutado nuevamente (años): </span>{{ test.recurring_time }}</span>
-
-    <span><span text-primary>¿Es posible navegar por las diferentes series del test?: </span>{{ test.navigable?'Sí':'No' }}</span>
-    <span><span text-primary>¿Es necesario chequear el completamiento para que el procesamiento del test sea correcto?: </span>{{ test.completed?'Sí':'No' }}</span>
-    <span><span text-primary>Tiempo de Test: </span>{{ test.type_psi_test.type_test_name }}</span>
-
+    
   </div>
 </template>
 <script setup lang="ts">
